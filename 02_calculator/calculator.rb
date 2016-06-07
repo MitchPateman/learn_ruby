5566567bde77654bea5234d13151b73db10a6191
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-07 16:30:34 -0400

    in calculator.rb, added multiply and multiply_more

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -8,7 +8,15 @@ end
 
 # sum takes an *array* of numbers and adds them all together
 def sum(sum)
-
 	sum.inject(0) { |total, number| total += number}
+end
 
+def multiply(x, y)
+	return x * y
+end
+
+def multiply_more(multiply_more)
+	total = 1.0
+	multiply_more.each { | number | total *= number }
+		 total
 end
