80a00cacba52f196a4075e06a720c287c092a249
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-07 21:00:25 -0400

    simon_says.rb, start_of_word works, 5 errors left.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,7 +1,22 @@
+require 'pry'
+
 def echo(word)
 	return word
 end
 
 def shout(word)
 	return word.upcase
+end
+
+def repeat(word, repeat_number = 2)
+	repeat_number.times.collect{word}.join(' ')
+	###Below works but has one extra " " after the last hello###
+	#return "#{word} " * repeat_number
+end
+
+def start_of_word(word, number_of_letters)
+	#binding.pry
+	split_word = word.split(//)
+	split_word = split_word[0...number_of_letters]
+	split_word * ""
 end
